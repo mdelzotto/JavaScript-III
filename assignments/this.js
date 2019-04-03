afc5cd7003c2e6868149a989c92973cc46e44635
mdelzotto <madelzotto@comcast.net>
2019-04-03 18:28:50 -0400

    My four principles for 'this' added

--- assignments/this.js
+++ assignments/this.js
@@ -1,12 +1,12 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
+ in your own words. explain the four principle for the "this" keyword below.
+
+ 1. Global Binding - When 'this' is not bound to anything, it will default to the window object.
+ 2. Implicit Binding - When a property is located inside of an object, it points to the parent object which is usually to the left of the '.' at call time.
+ 3. Explicit Binding - When we specify what 'this' is using call, apply, bind.
+ 4. New Binding - When we use a constructor function to create a new object.
+
+ write out a code example of each explanation above
 */
 
 // Principle 1
